c68ac9349bd84ce56dac0e7c8379250c405fc466
lepelog <lepelog4@gmail.com>
2020-04-01 23:43:30 +0200

    set better AMOUNT_PRESETS

--- bundles/tracker/donation/DonationConstants.ts
+++ bundles/tracker/donation/DonationConstants.ts
@@ -13,7 +13,7 @@ export const EMAIL_OPTIONS = [
   },
 ];
 
-export const AMOUNT_PRESETS = [25, 50, 75, 100, 250, 500];
+export const AMOUNT_PRESETS = [5, 10, 15, 25, 50, 100];
 
 export const MAX_BIDS_PER_DONATION = 10;
 export const BID_MINIMUM_AMOUNT = 1.0;
